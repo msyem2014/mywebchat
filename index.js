0325222f55dc76bb0119280f23c7dc135df6d1e4
msyem2014 <msyem2014@gmail.com>
2018-01-30 02:03:52 +0200

    inital commit to read

--- index.js
+++ index.js
@@ -2,5 +2,10 @@ const express =require('express');
 const app = express();
 app.set('view engine','ejs');
 app.set('views','./views');
-app.get('/',(req,res)=>res.render('home'));
+app.get('/',(req,res)=>{
+
+
+    res.render('home')
+
+});
 app.listen(3000,()=>console.log('Server start'));
